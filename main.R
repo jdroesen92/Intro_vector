e9ac633cef1a7a9d6a9b294b35ae1eaa40bd5635
jdroesen92 <jan.droesen@wur.nl>
2016-01-11 17:30:11 +0100

    Draft version

---
+++ main.R
@@ -0,0 +1,50 @@
+#Careli Caballero and Jan Droesen
+#Team  CarJan
+#01-11-2016
+
+#
+library(sp)
+library(raster)
+library(rgeos)
+
+#download the files
+railwayShp <- shapefile('data/Railways/railways.shp')
+placesShp <- shapefile('data/Places/places.shp')
+
+#visual check of shapefiles
+plot(railwayShp)
+plot(placesShp)
+
+#select industrial railways type == "industrial"
+industrialrail <- railwayShp[railwayShp$type == "industrial",]
+
+#project industrialrail and places in RDnew
+prj_string_RD <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +units=m +no_defs")
+(placesRD <- spTransform(placesShp, prj_string_RD))
+(railRD <- spTransform(industrialrail, prj_string_RD))
+
+#create buffer around industrial rails with 1000 m
+bufferRail <- gBuffer(railRD, byid=TRUE, width=1000)
+
+#intersect the buffer with cities
+
+row.names(gIntersection(placesRD, bufferRail, byid=TRUE))
+intersect <- strsplit(row.names(gIntersection(placesRD, bufferRail, byid=TRUE)), split = ' ')
+citycode <- as.numeric(intersect[[1]][1])
+
+#Create a plot that shows the buffer, the points, and the name of the city
+plot(bufferRail)
+plot(intersect, add=TRUE)
+#add labels to plot and maybe visualize
+
+#write down the name of the city and the population of that city as one comment at the end of the script.
+pop <- (paste("Population =", placesRD[5973,]$population))
+paste("Population =", placesRD[5973,]$population)
+paste("Cityname =", placesRD[5973,]$name)
+
+
+
+
+
+
+
